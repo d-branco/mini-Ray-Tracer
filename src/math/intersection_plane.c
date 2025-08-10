139739230f1132afe9f37036ca5f84bf04ae7694
gris <185072671+d-branco@users.noreply.github.com>
2025-08-10 10:08:40 +0100

    made planes opaque

--- src/math/intersection_plane.c
+++ src/math/intersection_plane.c
@@ -34,18 +34,15 @@ int	smll_dst_to_plane(t_scene *rt, t_tuple dir, t_lst_obj *pl, float *dst)
 int	pl_intersect(t_scene *rt, t_tuple pnt, t_tuple dir, t_lst_obj *pl)
 {
 	float	dst;
+	float	denom;
 
 	(void) rt;
-	if (fl_equal(vec_inner_product(dir, pl->vec_uni), 0))
-		return (FALSE);
-	if (fl_equal(vec_inner_product(
-				pnt_subtraction_into_vec(pnt, pl->center), pl->vec_uni), 0))
+	denom = vec_inner_product(dir, pl->vec_uni);
+	if (fl_equal(denom, 0))
 		return (FALSE);
 	dst = vec_inner_product(pl->vec_uni,
-			pnt_subtraction_into_vec(pl->center, pnt))
-		/ vec_inner_product(
-			pnt_subtraction_into_vec(pnt, pl->center), pl->vec_uni);
-	if (dst < 0)
+			pnt_subtraction_into_vec(pl->center, pnt)) / denom;
+	if (dst <= EPSILON || dst >= 1.0f)
 		return (FALSE);
 	return (TRUE);
 }
